0154fc5f00fc64479e85e1c64d2c027ed8deeca6
Thisarahetz <thisarayashodagcc@gmail.com>
2021-01-03 00:05:09 +0530

    my-1

--- src/Componet/NameList/NameList.jsx
+++ src/Componet/NameList/NameList.jsx
@@ -94,25 +94,29 @@ function NameList(){
 
     const addUserHandlare = () =>{
       const newUser =  {
-        "name": {
-            "title": "mr",
-            "first": "brad",
-            "last": "gibson"
-          },
-          "location": {
-            "street": "9278 new road",
-            "city": "kilcoole",
-            "state": "waterford",
-            "postcode": "93027",
-            },
-            "email": "brad.gibson@example.com",
-            "dob": {
-                "date": "1993-07-20T09:44:18.674Z",
-                "age": 26
-              },
-              "picture": {
-                "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
-              }           
+          "name": {
+          "title": "mr",
+          "first": "braasdsadsd",
+          "last": "thisara"
+        },
+        "location": {
+          "street": "9278 new road",
+          "city": "kilcoole",
+          "state": "waterford",
+      },
+      "email": "brad.gibson@example.com",
+      "dob": {
+        "date": "1993-07-20T09:44:18.674Z",
+        "age": 26
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/76.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/76.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      
+      
+                
     };
     setnameList((nameList) => nameList.concat(newUser));
     //setnameList(nameList.concat(newUser));
